62497f0218fef4ab0e2b6d17bf6ceecf8bf1c709
tokidoki-53 <v.asurada.11@gmail.com>
2019-08-26 15:45:49 +0900

    【デプロイ作業】S3の設定【Re:12】

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'tokidoki-test'
-  # config.asset_host = 'https://tokidoki-test.s3-ap-northeast-1.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tokidoki-test'
 end
