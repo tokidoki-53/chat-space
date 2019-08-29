c6b8431cc094a3ad81a013030267c30815a9bccb
tokidoki-53 <v.asurada.11@gmail.com>
2019-08-30 00:58:15 +0900

    【デプロイ作業】S3の設定【Re:18】

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
+
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
@@ -12,6 +11,8 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_directory  = 'tokidoki-test'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tokidoki-test'
 end
